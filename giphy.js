fba3a3f07ddc2c5a8b130e13fcf7ec146b105a28
Bailey Beck <baileymbeck@gmail.com>
2019-09-18 14:29:18 -0500

    added button render function

--- giphy.js
+++ giphy.js
@@ -15,6 +15,48 @@ function plantPics(){
 // formula to build api request
 var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + plantInput + "&limit=10&offset=0&rating=G&lang=en";
 var plant = $(this).attr("data-type");
+}
+
+// create new plant buttons
+function renderButtons() {
+
+    // empty button list
+    $("#buttons-view").empty();
+
+// create a for loop to pull from array
+for (var i = 0; i < plants.length; i++) {
+      
+          // create the button start and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("plant");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-name", plants[i]);
+          // Providing the button's text with a value of the plant at index i
+          a.text(plants[i]);
+          // Adding the button to the HTML
+          $("#buttons-view").append(a);
+        }
+};
+// add plant button function
+    $("#add-movie").on("click", function(event) {
+
+// event.preventDefault() prevents the form from trying to submit itself.
+    event.preventDefault();
+
+// This line will grab the text from the input box
+var plant = $("#plant-input").val().trim();
+
+// The plant name from the textbox is then added to our array
+plants.push(plant);
+
+// calling renderButtons which handles the processing of our movie array
+renderButtons();
+});
+
+// Calling the renderButtons function at least once to display the initial list of plants
+renderButtons();
+
 
 // ajax pull request
 $.ajax({
@@ -25,5 +67,5 @@ $.ajax({
 
 
 });
-}
+
 
