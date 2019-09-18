f9e5042c3e7b9b73544401635e149a4b5dbd2b4a
Bailey Beck <baileymbeck@gmail.com>
2019-09-17 19:33:27 -0500

    set full api key variables

--- giphy.js
+++ giphy.js
@@ -1,1 +1,11 @@
-var apiKey = "DEnzkZJKkCehMUYpZJBk93I8JrMrXOpp";
+// full request for plant gifs
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DEnzkZJKkCehMUYpZJBk93I8JrMrXOpp&q=plants&limit=25&offset=0&rating=G&lang=en";
+
+// api key + userInput "&q="
+var apiKey = "DEnzkZJKkCehMUYpZJBk93I8JrMrXOpp&q=";
+// what the user can choose to add
+var userInput = "";
+
+// formula to build api request
+var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + userInput + "&limit=10&offset=0&rating=G&lang=en";
+
